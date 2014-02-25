02deb335f94ac911d3ea96cacd7fc7fd4f45685b
Zach Goldman <elevatorgoldman@aol.com>
2014-02-25 23:13:02 +0000

    i just added state machines to models

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,7 @@
 class Hotel < ActiveRecord::Base
   attr_accessible :address, :city, :email, :free_breakfast, :gym, :hot_tub, :name, :phone, :pool, :state, :website
+  
+  state_machine :initial => :vacancies do
+    
+  end  
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ActiveRecord::Base
   belongs_to :hotel
   attr_accessible :bedrooms, :beds, :booking_status, :fridge, :microwave, :name, :nightly_rate, :sauna, :smoke_free, :television
+  
+  state_machine :initial => :empty do
+    
+  end
 end
