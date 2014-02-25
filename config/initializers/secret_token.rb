92570cd25c8550bb48481d9e857226e8d66df320
Zach Goldman <elevatorgoldman@aol.com>
2014-02-25 23:33:35 +0000

    i just updated all the things

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HotelsSendgrid::Application.config.secret_token = 'ba0625598cab6952710a708fe4e24f74cef286f7fd058b3e3fb5d987b5d1bbea7ba3b874fa6f67b9a4fa4040032c67492f28741ed80b4f31935fa704039cc1a0'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HotelsSendgrid::Application.config.secret_key_base = 'ade7ac2b2bb3dcc71ae3c62060e99c47e27b70b07abcfcb72896705806c9ab6fb94b1bcdbd9a81c815bc555b68d0809d5d9d0b290d63d7404ba72d5d370c29ab'
