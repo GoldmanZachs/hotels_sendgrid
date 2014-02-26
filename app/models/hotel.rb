ba90a54ab538ec5b0413fd6fd1bf5ee24c0e9666
Zach Goldman <elevatorgoldman@aol.com>
2014-02-26 00:07:02 +0000

    i just added fix to hotel

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,5 @@
 class Hotel < ActiveRecord::Base
+  has_many :rooms, dependent: :destroy
   attr_accessible :address, :city, :email, :free_breakfast, :gym, :hot_tub, :name, :phone, :pool, :state, :website
   
   state_machine :initial => :vacancies do
