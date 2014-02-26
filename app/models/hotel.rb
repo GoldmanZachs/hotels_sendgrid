739150c4ad34c20da019f432df31ff9577ea2d74
Zach Goldman <elevatorgoldman@aol.com>
2014-02-26 00:57:38 +0000

    i just added trannies to state in models

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -4,6 +4,8 @@ class Hotel < ActiveRecord::Base
   #attr_accessible :address, :city, :email, :free_breakfast, :gym, :hot_tub, :name, :phone, :pool, :state, :website
   
   state_machine :initial => :vacancies do
-    
+    event :last_room_filled do
+      transition :vacancies => :no_vacancies
+    end    
   end  
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,8 @@ class Room < ActiveRecord::Base
   #attr_accessible :bedrooms, :beds, :booking_status, :fridge, :microwave, :name, :nightly_rate, :sauna, :smoke_free, :television
   
   state_machine :initial => :empty do
-    
+    event :booked do
+      transition :empty => :occupied
+    end      
   end
 end
