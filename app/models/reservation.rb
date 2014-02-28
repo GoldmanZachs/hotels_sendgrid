5e400846c3d4648ace73d789792c64c37e8bb084
Zach Goldman <elevatorgoldman@aol.com>
2014-02-28 08:02:13 +0000

    i just state trannies to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,25 @@ class Reservation
   include Mongoid::Document
 
   field :reserved_by, type: String
-  field :reserved_on, type: Date
+  field :reserved_on, type: Date 
+  field :reserved_to, type: Date
+  
+  field :state
 
   belongs_to :room, after_add: :send_email
+  
+  aasm do
+    state :available, initial: true
+    state :reserved
+    
+    event reserve_room do
+      before { self.reserved_on = Date.today } 
+      transitions from: :available, to: :reserved
+    end  
+    
+    event check_out do
+      after { self.reserved_to = Date.today } 
+      transitions from: :reserved, to: :available
+    end  
+  end
 end
